33c5877c31d2af5a97704ec5624efc4707cb2277
Volo-xx <waldeksudol@poczta.onet.pl>
2024-03-13 21:46:21 +0100

    Update select.py

--- custom_components/tuya_ble/select.py
+++ custom_components/tuya_ble/select.py
@@ -104,6 +104,26 @@ mapping: dict[str, TuyaBLECategorySelectMapping] = {
             ),
         }
     ),
+    "jtmspro": TuyaBLECategorySelectMapping(
+        products={
+            "rlyxv7pe":  # Smart Lock
+            [
+                TuyaBLESelectMapping(
+                    dp_id=31,
+                    description=SelectEntityDescription(
+                        key="beep_volume",
+                        options=[
+                            "mute",
+                            "low",
+                            "normal",
+                            "high",
+                        ],
+                        entity_category=EntityCategory.CONFIG,
+                    ),
+                ),
+            ]
+        }
+    ),
     "szjqr": TuyaBLECategorySelectMapping(
         products={
             **dict.fromkeys(
